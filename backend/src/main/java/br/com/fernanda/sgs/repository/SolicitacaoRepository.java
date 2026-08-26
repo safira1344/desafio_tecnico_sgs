7db6ba765e16dad6e69b28899f0eb618be1e48df
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-08-26 20:01:24 -0300

    fix: ajustar a consulta na interface SolicitacaoRepository para retornar objetos do tipo SolicitacaoListagemDTO

--- backend/src/main/java/br/com/fernanda/sgs/repository/SolicitacaoRepository.java
+++ backend/src/main/java/br/com/fernanda/sgs/repository/SolicitacaoRepository.java
@@ -4,26 +4,29 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import br.com.fernanda.sgs.model.Solicitacao;
+import br.com.fernanda.sgs.model.SolicitacaoListagemDTO;
 
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Integer> {
     @Query(value = """
-    SELECT s.id, s.descricao, s.valor, s.data_solicitacao, s.status,
-        sol.nome as nome_solicitante, sol.cpf_cnpj,
-        c.nome as nome_categoria
-    FROM solicitacao s
-    JOIN solicitante sol ON sol.id = s.solicitante_id
-    JOIN categoria c ON c.id = s.categoria_id
-    WHERE (:status IS NULL OR s.status = :status) 
-    AND (:categoriaId IS NULL OR c.id = :categoriaId)
-    AND (:dataInicio IS NULL OR s.data_solicitacao >= :dataInicio)
-    AND (:dataFim IS NULL OR s.data_solicitacao <= :dataFim)
-    """, nativeQuery = true)
-
-    List<Object[]> listarComFiltros(
-    @Param("status") String status,
-    @Param("categoriaId") Integer categoriaId,
-    @Param("dataInicio") LocalDate dataInicio,
-    @Param("dataFim") LocalDate dataFim
-);
+        select s.id, s.descricao, s.valor, s.data_solicitacao, s.status,
+            sol.nome as nome_solicitante, sol.cpf_cnpj,
+            c.nome as nome_categoria
+        from solicitacao s
+        join solicitante sol on sol.id = s.solicitante_id
+        join categoria c on c.id = s.categoria_id
+        where (:status is null or s.status = :status) 
+        and (:categoriaId is null or c.id = :categoriaId)
+        and (:dataInicio is null or s.data_solicitacao >= :dataInicio)
+        and (:dataFim is null or s.data_solicitacao <= :dataFim)
+        """, nativeQuery = true)
+    List<SolicitacaoListagemDTO> listarComFiltros(
+        @Param("status") String status,
+        @Param("categoriaId") Integer categoriaId,
+        @Param("dataInicio") LocalDate dataInicio,
+        @Param("dataFim") LocalDate dataFim
+    );
 }

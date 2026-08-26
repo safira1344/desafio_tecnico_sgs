24eee306149a695b000d85063efa51044a11d026
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-08-25 22:39:54 -0300

    add: as classes SgsApplication e SgsApplicationTests para o ponto de entrada da aplicação e para testes.

---
+++ backend/src/main/java/br/com/fernanda/sgs/SgsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.sgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/fernanda/sgs/SgsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.sgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
